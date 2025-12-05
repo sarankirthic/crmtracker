5377acaa6127347aba451921b738bf5376bcc906
saran <spskartist@gmail.com>
2025-12-05 17:18:25 +0530

    added requirements.txt file

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost:5432/clientdb'
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'postgresql://localhost:5432/clientdb')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
